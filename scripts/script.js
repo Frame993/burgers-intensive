56ff6769165ed7d9ef315b19d3195e4fce1b6af3
Yordani <yalvarezb93@gmail.com>
2024-02-19 12:39:20 +0300

    js interactions done

---
+++ scripts/script.js
@@ -0,0 +1,67 @@
+// main-action
+document.getElementById('main-action-button').onclick = function () {
+    document.getElementById('products').scrollIntoView({ behavior: 'smooth' })
+}
+// menu-item
+let links = document.querySelectorAll('.menu-item > a')
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+        document.getElementById(links[i].getAttribute('data-link')).scrollIntoView({ behavior: 'smooth' })
+    }
+}
+
+// product-button
+let buttons = document.getElementsByClassName('product-button')
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById('order').scrollIntoView({ behavior: 'smooth' })
+    }
+}
+
+// order-action
+let burger = document.getElementById('burger')
+let nameO = document.getElementById('name')
+let phone = document.getElementById('phone')
+
+document.getElementById('order-action').onclick = function () {
+    let hasError = false;
+
+    [burger, nameO, phone].forEach(item => {
+        if (!item.value) {
+            item.parentElement.style.background = 'red'
+            hasError = true
+        } else {
+            item.parentElement.style.background = ''
+        }
+    })
+
+    if (!hasError) {
+        [burger, nameO, phone].forEach(item => {
+            item.value = ''
+        })
+        alert('Спасибо за заказ!,Мы cкоро свяжемся с вами!')
+    }
+}
+
+// currency
+let prices = document.getElementsByClassName('products-item-price')
+document.getElementById('change-currency').onclick = function (e) {
+    let currentCurrency = e.target.innerText
+
+    let newCurrency = '$'
+    let coefficient = 1
+
+    if (currentCurrency === '$') {
+        newCurrency = '₽'
+        coefficient = 80
+    }
+    else if (currentCurrency === '₽') {
+        newCurrency = 'BYN'
+        coefficient = 3
+    }
+    e.target.innerText = newCurrency
+
+    for (let i = 0; i < prices.length; i++) {
+     prices[i].innerText = +(prices[i].getAttribute('data-base-price') * coefficient).toFixed(1) + " " + newCurrency
+    }
+}
